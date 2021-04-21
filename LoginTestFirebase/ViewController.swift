b0faea970067eba507a1aeb133ab61cb2b102da4
Yuta iwasaki <yanyong0127@gmail.com>
2021-04-22 00:52:38 +0900

    Adding animations to the keyboard

--- LoginTestFirebase/ViewController.swift
+++ LoginTestFirebase/ViewController.swift
@@ -26,6 +26,28 @@ class ViewController: UIViewController {
         emailTextField.delegate = self
         passwordTextField.delegate = self
         userNameTextField.delegate = self
+        //キーボードの認識
+        NotificationCenter.default.addObserver(self, selector: #selector(showkeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(hidekeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func showkeyboard(notification: Notification){
+        //キーボードのフレームを求める
+        let keyboardFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+        //https://qiita.com/st43/items/3802624d15a8dded8169 //フレームについて
+        guard let kayboardMinY = keyboardFrame?.minY else { return } //キーボードの高さ
+        let registerButtonMaxY = registerButton.frame.maxY //registerButtonの底辺の位置
+        let distance = registerButtonMaxY - kayboardMinY + 30
+        let transform = CGAffineTransform(translationX: 0, y: -distance)
+        //https://qiita.com/hachinobu/items/57d4c305c907805b4a53 //Animation
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+            self.view.transform = transform
+        })
+        //print("kayboardMinY: \(String(describing: kayboardMinY)), registerButtonMaxY: \(registerButtonMaxY)")
+    }
+    @objc func hidekeyboard(){
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+            self.view.transform = .identity
+        })
     }
     //画面をタップした時にキーボードを閉じる
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
